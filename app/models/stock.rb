e5d56f2d9bca5453356a699c869b00a2b91de309
Pratik Shah <pratik.t.shah@gmail.com>
2016-01-23 22:29:45 -0600

    Before save uppercase field

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,8 @@
 class Stock < ActiveRecord::Base
+  # validate there is a ticker and that ticker is not in db regardless of its case
   validates :ticker, { presence: true, :uniqueness => { :case_sensitive => false } }
 
-  before_save :uppercase_field
+  before_save :uppercase_field  # before saving uppercase all letters
 
   def uppercase_field
     self.ticker.upcase!
